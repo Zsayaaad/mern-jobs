26f296db5eab70921bbdf5c233d9d89fc093e6aa
Ziad Elsayyad <ziad.elsyad16@gmail.com>
2026-04-21 23:00:14 +0200

    - using errorElement to show error page when enter incorrect route - using Outlet in parent route to render children routes

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
     <div>
-      <h1>HomeLayout Page</h1>
-      <Link to="/register">Register Page</Link>
+      <nav>navbar</nav>
+      <Outlet />
     </div>
   );
 };
